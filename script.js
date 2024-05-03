90cf4707372e4172058125b6188a9c267ab3302e
MohammedSIssa <47881107+MohammedSIssa@users.noreply.github.com>
2024-05-03 17:35:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ left2right.addEventListener('click', ()=>{
     let eqn = equation.value
     let numbers = eqn.match(numsRegEx)
     let ops = eqn.match(opsRegEx)
-    let ans1 = (ops[0] == "*") ? parseInt(numbers[0]) * parseInt(numbers[1]) : parseInt(numbers[0]) / parseInt(numbers[1])
-    answer.textContent = (ops[1] == "*") ? ans1 * parseInt(numbers[2]) : ans1 / parseInt(numbers[2])
+    let ans1 = (ops[0] == "*") ? parseFloat(numbers[0]) * parseFloat(numbers[1]) : parseFloat(numbers[0]) / parseFloat(numbers[1])
+    answer.textContent = (ops[1] == "*") ? ans1 * parseFloat(numbers[2]) : ans1 / parseFloat(numbers[2])
 })
 
 right2left.addEventListener('click', ()=>{
     let eqn = equation.value;
     let numbers = eqn.match(numsRegEx)
     let ops = eqn.match(opsRegEx)
-    let ans1 = (ops[1] == "*") ? parseInt(numbers[2]) * parseInt(numbers[1]) : parseInt(numbers[2]) / parseInt(numbers[1])
-    answer.textContent = (ops[0] == "/") ? ans1 / parseInt(numbers[0]) : ans1 * parseInt(numbers[0])
+    let ans1 = (ops[1] == "*") ? parseFloat(numbers[2]) * parseFloat(numbers[1]) : parseFloat(numbers[2]) / parseFloat(numbers[1])
+    answer.textContent = (ops[0] == "/") ? ans1 / parseFloat(numbers[0]) : ans1 * parseFloat(numbers[0])
 })
